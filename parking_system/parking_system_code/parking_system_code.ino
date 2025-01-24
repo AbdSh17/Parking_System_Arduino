613f3d99d8a0f78b120778c9f7fd58900ce6afc4
AbdSh17 <abdalraheem200417a@gmail.com>
2025-01-24 23:04:16 +0200

    do the whole thing into functions

--- parking_system/parking_system_code/parking_system_code.ino
+++ parking_system/parking_system_code/parking_system_code.ino
@@ -41,41 +41,16 @@ void setup() {
 
 void loop() {
 
-  int potentiometerValue = analogRead(potentiometerPin);
-  int potentiometerFlag = 0;
+  potentiometer();
+
+  distanceToOpen();
+
+  keypadOpen();
   
-  while (potentiometerValue <= 1000) { // stay forever until the potentiometer is at default
-    if (!potentiometerFlag) { // if it's the first time entering the loop, restart everything
-      analogWrite(motor_pin, close_gate);
-      potentiometerFlag = 1;
-      delay(500);
-    }
-    analogWrite(motor_pin, map(potentiometerValue, 0, 1023, 0, 240)); // note that 240 is not the max but it's the best value with respect to the angle and speed specifics
-    delay(50);
-    potentiometerValue = analogRead(potentiometerPin);
-  }
 
-  // restart the trigger, then start it again then restart it, (neg_edge, pos_edge, neg_edge)
-  digitalWrite(trigPin, 0);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, 1);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, 0);
- 
-  duration = pulseIn(echoPin, 1); // duration: The time (in microseconds) that the echo signal remains HIGH on the echoPin
-  distance = (duration*.0343)/2; // (343 meters/second) speed of the light in the air, (2) measure the distance for one way
-   Serial.print("Distance: ");
-   Serial.println(distance);
-  delay(10);
+}
 
-  if (distance <= distance_to_open) { 
-    analogWrite(motor_pin, open_gate);
-    delay(waitTime);
-  }
-  else {
-    analogWrite(motor_pin, close_gate);
-  }
-
+void keypadOpen(){
   char key = keypad.getKey(); // get the key the user entered
   if (userIndex == 5)
     userIndex = 0;
@@ -97,7 +72,45 @@ void loop() {
       printString(userPass, userIndex);
     }
   }
+}
 
+void distanceToOpen(){
+    // restart the trigger, then start it again then restart it, (neg_edge, pos_edge, neg_edge)
+  digitalWrite(trigPin, 0);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, 1);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, 0);
+ 
+  duration = pulseIn(echoPin, 1); // duration: The time (in microseconds) that the echo signal remains HIGH on the echoPin
+  distance = (duration*.0343)/2; // (343 meters/second) speed of the light in the air, (2) measure the distance for one way
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(10);
+
+  if (distance <= distance_to_open) { 
+    analogWrite(motor_pin, open_gate);
+    delay(waitTime);
+  }
+  else {
+    analogWrite(motor_pin, close_gate);
+  }
+}
+
+void potentiometer() {
+  int potentiometerValue = analogRead(potentiometerPin);
+  int potentiometerFlag = 0;
+  
+  while (potentiometerValue <= 1000) { // stay forever until the potentiometer is at default
+    if (!potentiometerFlag) { // if it's the first time entering the loop, restart everything
+      analogWrite(motor_pin, close_gate);
+      potentiometerFlag = 1;
+      delay(500);
+    }
+    analogWrite(motor_pin, map(potentiometerValue, 0, 1023, 0, 240)); // note that 240 is not the max but it's the best value with respect to the angle and speed specifics
+    delay(50);
+    potentiometerValue = analogRead(potentiometerPin);
+  }
 }
 
 int comapreStrings(char *str1, char *str2, int length) {
